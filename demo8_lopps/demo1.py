36a19bc27d35d42e94e78c07ccbbc8e3a0ccacbb
Sanket Chavan <Sanket.Chavan@einfochips.com>
2023-02-22 12:32:47 +0530

    python project: first commit

---
+++ demo8_lopps/demo1.py
@@ -0,0 +1,7 @@
+numbers=[45,98,75,65,24,88,74,56,75]
+for number in range(0, len(numbers)):
+    if numbers[number] ==
+
+colors = ["red", "green", "yellow", "blue", "green", "black", "grey"]
+for color in colors:
+    print(color)
